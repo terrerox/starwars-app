05b6934bd7c585384db832e1f8f138cc2438deed
Francis Terrero <terrero0611@gmail.com>
2022-07-28 17:16:52 +0200

    search input

--- src/components/home/HomeScreen.tsx
+++ src/components/home/HomeScreen.tsx
@@ -9,16 +9,41 @@ const url = 'https://swapi.dev/api/people'
 
 export const HomeScreen = () => {
   const { data, loading } = useFetch<PeopleResponseFromAPI>(url)
+  const [people, setPeople] = useState<Array<Person> | undefined>([])
+  const [inputValue, setInputValue] = useState<string>("")
+
+  useEffect(() => {
+    setPeople(data?.results)
+  
+  }, [data])
+
+  useEffect(() => {
+    const filterByName = (): void => { 
+      const value = inputValue.trim().toLowerCase()
+      setPeople(
+        people?.filter(person => { 
+          const name = person.name.toLowerCase()
+          return name.includes(value) 
+        })
+      ) 
+    }
+    filterByName()
+  
+  }, [inputValue])
+  
   
   return (
-    <div className="App">
-      <div className="cards">
+    <main className="App">
+      <section className="filter">
+        <input type="text" placeholder="Find by name" onChange={e => setInputValue(e.target.value)}/>
+      </section>
+      <section className="cards">
         {
           !data && <p>Loading...</p>
         }
        {
            data && (
-             data?.results.map(p => (
+            people?.map(p => (
               <Card 
                 key={p.name} 
                 person={p}
@@ -26,8 +51,8 @@ export const HomeScreen = () => {
             ))
           )
        } 
-      </div>
-    </div>
+      </section>
+    </main>
   )
 }
 
